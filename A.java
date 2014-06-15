f62115f0c34cd625fd037cb21e1fdfd7a18363ea
Joseph Thuemler <joethuemler@gmail.com>
2014-06-14 19:58:25 -0700

    Getting ready for the actual contest
    Delete the practice stuff

---
+++ A.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class A {
+  public static void main(String[] args) throws IOException {
+    Scanner in = new Scanner(new File("a1.in"));
+    PrintWriter out = new PrintWriter(new File("a1.out"));
+  }
+}
